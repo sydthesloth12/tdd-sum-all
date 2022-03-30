21b0907267c5c16d0037d2743d8627c566bede75
Sydney <sydneysirois17@gmail.com>
2022-03-29 21:57:53 -0400

    Two Tests added

---
+++ tests/calculation.test.js
@@ -0,0 +1,32 @@
+const describe = require('mocha').describe
+
+const it = require('mocha').it
+
+const expect = require('chai').expect
+
+const strictEqual = require('../calculation')
+
+describe('calculation.js test suite', () => {
+    describe('strictEqual tests', () => {
+
+    it('returns the value of a false array', () => {
+        const mockData = ["5", 5]
+        const expectedResult = false
+    
+        const actualResult = strictEqual(mockData)
+
+        expect(actualResult).to.be.equal(expectedResult)
+    })
+
+    it('returns value of true array', () => {
+        const mockData = [5, 5]
+        const expectedResult = true
+
+        const actualResult= strictEqual(mockData)
+
+        expect(actualResult).to.be.equal(expectedResult)
+
+    })
+
+    })
+})
